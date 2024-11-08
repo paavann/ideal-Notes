48b34649f34300fabbd1719910a5b4005ee7ece4
paavann <pavann22826@gmail.com>
2024-11-08 12:11:15 +0530

    404 handling

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3500
 
 
 //middleware - app.use()
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 
@@ -17,6 +17,8 @@ app.all('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'views', '404.html'));
     } else if (req.accepts('json')) {
         res.status(404).json({ message : "requested page does not exist" });
+    } else {
+        res.type('txt').send("404 not found")
     }
 })
 
